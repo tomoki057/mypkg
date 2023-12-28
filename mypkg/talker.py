6ed124b970623afa026a8d5a4aa6210d9b4b1bcf
Tomoki sato <s22C1057KW@s.chibakoudai.jp>
2023-12-29 01:46:26 +0900

    first commit

---
+++ mypkg/talker.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2023 Tomoki sato
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+rclpy.init()
+node = Node("talker")
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
+
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
+rclpy.spin(node)
